d8f2f9db01f909b04b903218ded2b0822a1fac2e
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-10-29 09:29:53 +0700

    menghapus bagian tidak perlu

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -1,13 +1,8 @@
-import * as React from "react"
-
-import { SearchForm } from "@/components/search-form"
-import { VersionSwitcher } from "@/components/version-switcher"
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
