561320164d17714dd7a8b105dd3690e80a611305
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-11-09 13:55:26 +0700

    menambahkan data kota

--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -1,171 +1,58 @@
-import {Card} from "@/components/ui/card.jsx";
-import {Button} from "@/components/ui/button.jsx";
-import {Phone} from "lucide-react";
+import { Card } from "@/components/ui/card.jsx";
+import { Button } from "@/components/ui/button.jsx";
+import { Phone, MapPin } from "lucide-react";
 
 export default function Marketplace() {
-    return (
-        <div className={'flex w-full items-center p-5 gap-5 flex-wrap'}>
-            <Card className={'flex flex-col w-[160px] md:w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
+  const data = [
+    {
+      gambar: "src/assets/temp/jualan1.png",
+      nama_produk: "Bibit padi unggul kualitas super.",
+      harga: "Rp300.000,-",
+      nama_toko: "UD. Gabriel",
+      lokasi: "Sibolga",
+    },
+  ];
 
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
+  return (
+    <div className={"flex w-full items-center p-5 gap-5 flex-wrap"}>
+      {data.map((item, index) => (
+        <Card
+          key={index}
+          className={
+            "flex flex-col w-[160px] md:w-full max-w-[250px] p-5 gap-3"
+          }
+        >
+          <div>
+            <img
+              className={"w-full"}
+              src={item.gambar}
+              alt={"petani"}
+              width={500}
+              height={500}
+            />
+          </div>
 
-                <div>
-                    <Button>
-                        <span className={'text-xs flex gap-1'}><Phone/> Chat Penjual</span>
-                    </Button>
-                </div>
-            </Card>
+          <div>
+            <span className={"text-gray-500 font-extralight block"}>
+              {item.nama_produk}
+            </span>
+            <span className={"font-bold text-sm"}>{item.harga}</span>
+          </div>
 
-            <Card className={'flex flex-col w-[160px] md:w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan2.webp'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Pupuk cair padi bunting ampuh.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-            <Card className={'flex flex-col w-full max-w-[250px] p-5 gap-3'}>
-                <div>
-                    <img className={'w-full'} src={'src/assets/temp/jualan1.png'} alt={'petani'} width={500} height={500}/>
-                </div>
-
-                <div>
-                    <span className={'text-gray-500 font-extralight block'}>Bibit padi unggul kualitas super.</span>
-                    <span className={'font-bold text-sm'}>Rp300.000,-</span>
-                </div>
-
-                <div>
-                    <Button>
-                        <Phone/>
-                        Chat Penjual
-                    </Button>
-                </div>
-            </Card>
-
-        </div>
-    )
+          <div>
+            <Button>
+              <span className={"text-xs flex gap-1"}>
+                <Phone /> Chat Penjual
+              </span>
+            </Button>
+            <span
+              className={"text-gray-500 text-sm flex mt-3 items-center gap-1"}
+            >
+              <MapPin size={16} /> {item.lokasi}
+            </span>
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
 }
