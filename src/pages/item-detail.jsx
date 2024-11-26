c4ba3b992c794545896c79a5d8b6d38676ee11d6
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-11-26 14:14:10 +0700

    fix bug

--- src/pages/item-detail.jsx
+++ src/pages/item-detail.jsx
@@ -1,9 +1,10 @@
-import {useParams} from "react-router-dom";
-import {Button} from "@/components/ui/button.jsx";
-import {Phone} from "lucide-react";
-import {useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "@/components/ui/button.jsx";
+import { Phone } from "lucide-react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {apiUrl} from "@/env.js";
+import { apiUrl } from "@/env.js";
+import { formatRupiah } from "@/services/format-rupiah.js";
 
 export default function ItemDetail() {
   const { id } = useParams();
@@ -13,39 +14,48 @@ export default function ItemDetail() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/products/${id}`, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        const response = await axios.get(
+          `http://localhost:3000/api/products/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
           },
-        });
+        );
         setProduct(response.data.data[0]);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     };
 
     fetchProducts().then();
-  }, [])
+  }, []);
 
   if (!product) return <p>Item tidak ditemukan</p>;
 
-  const { nama, picture, description, harga, lokasi, whatsapp_number, uploader_id } = product;
+  const {
+    nama,
+    picture,
+    description,
+    harga,
+    lokasi,
+    whatsapp_number,
+    uploader_id,
+  } = product;
 
   // Pesan template yang akan muncul di WhatsApp
   const templateMessage =
     "Halo, saya tertarik dengan produk ini yang ada di Forum Tani ID. Apakah produk ini masih tersedia?";
   const encodedMessage = encodeURIComponent(templateMessage);
 
-
   const handleChatClick = () => {
     const waLink = `https://wa.me/${whatsapp_number}?text=${encodedMessage}`;
-    window.open(waLink, "_blank"); 
+    window.open(waLink, "_blank");
   };
 
   return (
     <div className="flex w-full justify-center px-4">
       <div className="flex flex-col md:flex-row w-full overflow-hidden">
-  
         <div className="w-full md:w-1/2 p-4 md:p-8">
           <img
             src={`${apiUrl}/${picture}`}
@@ -54,17 +64,15 @@ export default function ItemDetail() {
           />
         </div>
 
-
         <div className="w-full md:w-1/2 p-4 md:p-8 flex flex-col gap-4 justify-center">
-  
           <h2 className="text-xl md:text-2xl font-semibold text-gray-800">
             {nama}
           </h2>
 
-   
-          <p className="text-2xl md:text-3xl text-red-500 font-bold">{harga}</p>
+          <p className="text-2xl md:text-3xl text-red-500 font-bold">
+            {formatRupiah(harga)},-
+          </p>
 
-  
           <div className="text-gray-500 text-sm md:text-base mt-2">
             <p>
               Dijual oleh:{" "}
@@ -76,7 +84,6 @@ export default function ItemDetail() {
             </p>
           </div>
 
-        
           <p className="text-gray-600 mt-4 leading-relaxed text-sm md:text-base">
             {description}
           </p>
